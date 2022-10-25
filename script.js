be12eb93a697080dd081f8920e3881261820bd31
JP-Repo-07 <jhonpaulriopay7@gmail.com>
2022-10-25 11:41:33 +0800

    JS Variables: Done

---
+++ script.js
@@ -0,0 +1,17 @@
+console.log("Hello World"); //String
+
+console.log(56); //numbers
+
+/* Multi Line
+Commemts */
+
+let name = "Paul";
+let age  = 23;
+age = 12;
+
+console.log(name);
+console.log(age);
+
+const color = "Red";
+//color = "white";
+console.log(color)
